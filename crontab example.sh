58444d8565074ab717dd72a06e176a4f1e6aca29
Emile <emile@ecspresso.com>
2021-12-05 22:37:56 +0100

    Create crontab example.sh

---
+++ crontab example.sh
@@ -0,0 +1,8 @@
+# m h  dom mon dow   command
+
+# Add domains to the blacklist at 22:00 every day and remove them again at 06:00 the day after.
+0 22 * * * /home/pi/dns.sh block   >> /home/pi/dns_$(date +\%Y\%m\%d).log
+0 6  * * * /home/pi/dns.sh unblock >> /home/pi/dns_$(date +\%Y\%m\%d).log
+
+# Remove all files older than 10 days.
+0 0 * * * find /home/pi/dns_* -type f -mtime +10 -delete
